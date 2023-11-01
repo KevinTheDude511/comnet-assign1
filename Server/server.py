0184778fb784362d9a817c0aa9a35df1049eb7a2
Minhkhoa444 <nguyenminhkhoa732@gmail.com>
2023-11-01 19:53:12 +0700

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -8,11 +8,29 @@ connectingClients = []
 def serverSend():
     while True:
         message = input()
+        message = message.strip().split(" ")
         if not connectingClients:
             print("There is no connecting client")
             continue
         for connectSocket in connectingClients:
-            connectSocket.send(message.encode())
+            ip, _ = connectSocket.getpeername()
+            if len(message) > 1:
+                if message[0] == "ping":
+                    if message[1] != ip:
+                        print(f"{message[1]} is down!")
+                        continue
+                    else:
+                        message = "requestPing"
+                elif message[0] == "discover":
+                    if message[1] != ip:
+                        print("Error!")
+                        continue
+                    else:
+                        message = "requestDiscover"
+                connectSocket.send(message.encode())
+            else:
+                print("Command missing arguments!")
+    connectSocket.close()
 
 def serverReceive(connectSocket, address):
     while True:
@@ -20,7 +38,15 @@ def serverReceive(connectSocket, address):
             message = connectSocket.recv(1024).decode()
             if not message:
                 break
+            command = message.strip().split(" ")
             print(f"{address}: {message}")
+            if (command[0] == "requestIP"):
+                connectSocket.send(("respondIP " + socket.gethostbyname(socket.gethostname())).encode())
+            elif command[0] == "respondPing":
+                print(f"{command[1]} is up.")
+            elif command[0] == "respondDiscover":
+                for i in command[1:]:
+                    print(i[:-1])
         except ConnectionResetError:
             break
     connectSocket.close()
@@ -53,4 +79,4 @@ def serverProgram():
     threadListen.start()
 
 if __name__ == '__main__':
-    serverProgram()
+    serverProgram()    serverProgram()
